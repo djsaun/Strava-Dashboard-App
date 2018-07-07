2f449feba748c165d84c5e978b5cf58661212976
David Saunders <david@monkee-boy.com>
2018-07-07 18:08:55 -0500

    Added JS date library to project; Format and add dates to activities

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import dateFns from 'date-fns'
 import '../css/Activity.css';
 
 class Activity extends Component {
@@ -6,6 +7,7 @@ class Activity extends Component {
     return(
       <div className="activity">
         <h3>{this.props.data.name}</h3>
+        <p className="date">{dateFns.format(this.props.data.start_date, 'MM/DD/YYYY')}</p>
         <p>Average Speed: {this.props.convertTime(this.props.data.average_speed)} mph</p>
         <p>Total Distance: {this.props.convertDistance(this.props.data.distance)} miles</p>
       </div>
